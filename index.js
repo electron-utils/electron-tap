37366ac06e1bb08aad7d1fb0b577f9847c1114fc
Johnny Wu <KarasAya@gmail.com>
2017-02-17 13:46:36 +0800

    update

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const globby = require('globby');
 const path = require('path');
 const chalk = require('chalk');
 
+yargs
+// .strict()
+.help('help')
+.version(app.getVersion())
+.usage('electron-tap <path> [options]')
+.options({
+  'renderer': { type: 'boolean', desc: 'Run tests in renderer.' },
+  'detail': { type: 'boolean', default: false, desc: 'Run test in debug mode (It will not quit the test, and open the devtools to help you debug it).' },
+  'reporter': { type: 'string', default: 'dot', desc: 'Test reporter, default is \'dot\'' },
+})
+;
+
+// parse the options in yargs
+// NOTE: this process will remove the app path from process.argv
+// WHY?
+// Usually we start electron application by: `electorn ./my-app --foo --bar`,
+// But some application (such as VS-Code) start it by: `electron --debug-brk="4032" ./my-app --foo --bar`
+// At the end it makes `yargs._` equals to [`./my-app`] which is unexpected for us.
+// NOTE: To understand why we use `yargs.parse()` instead of `yargs.argv`, see http://yargs.js.org/docs/#methods-argv
+let cwd = process.cwd();
+let argv = process.argv.slice(1);
+for ( let i = 0; i < argv.length; ++i ) {
+  if ( path.resolve(cwd, argv[i]) === app.getAppPath() ) {
+    argv.splice(i,1);
+    break;
+  }
+}
+
+let yargv = yargs.parse(argv);
+// console.log(yargv); // DEBUG
+
+// ==========================
+// internal
+// ==========================
+
 function _logError (text) {
   console.log(chalk.red(text));
 }
@@ -26,37 +61,6 @@ function _done (failures) {
 }
 
 app.on('ready', () => {
-  yargs
-    // .strict()
-    .help('help')
-    .version(app.getVersion())
-    .usage('electron-tap <path> [options]')
-    .options({
-      'renderer': { type: 'boolean', desc: 'Run tests in renderer.' },
-      'detail': { type: 'boolean', default: false, desc: 'Run test in debug mode (It will not quit the test, and open the devtools to help you debug it).' },
-      'reporter': { type: 'string', default: 'dot', desc: 'Test reporter, default is \'dot\'' },
-    })
-    ;
-
-  // parse the options in yargs
-  // NOTE: this process will remove the app path from process.argv
-  // WHY?
-  // Usually we start electron application by: `electorn ./my-app --foo --bar`,
-  // But some application (such as VS-Code) start it by: `electron --debug-brk="4032" ./my-app --foo --bar`
-  // At the end it makes `yargs._` equals to [`./my-app`] which is unexpected for us.
-  // NOTE: To understand why we use `yargs.parse()` instead of `yargs.argv`, see http://yargs.js.org/docs/#methods-argv
-  let cwd = process.cwd();
-  let argv = process.argv.slice(1);
-  for ( let i = 0; i < argv.length; ++i ) {
-    if ( path.resolve(cwd, argv[i]) === app.getAppPath() ) {
-      argv.splice(i,1);
-      break;
-    }
-  }
-
-  let yargv = yargs.parse(argv);
-  // console.log(yargv); // DEBUG
-
   // handle patterns
   let patterns = yargv._.map(pattern => {
     let stats = null;
